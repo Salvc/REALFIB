5c5ad2d5f611ce33994876f105c2d0e2ca255c05
Salvc <scamarote20@scienceleadership.org>
2020-02-07 09:51:37 -0500

    yes

--- src/Fibonaccii.java
+++ src/Fibonaccii.java
@@ -1,10 +1,12 @@
-public class Fibonaccii implements Fibonacci{
-    int amountofNumbersInSequence = 0;
-    int number1 = 0;
-    int number2 = 0;
+public class Fibonaccii implements Fibonacci, Runnable {
+    private int amountofNumbersInSequence = 0;
+    private int number1 = 0;
+    private int number2 = 0;
 
-    Fibonaccii(  int num1, int num2, int amountofNumbers    ) {
-        amountofNumbersInSequence = amountofNumbers;
+    private String sequence = "";
+
+    Fibonaccii(int num1, int num2, int amountofNumbers) {
+        this.amountofNumbersInSequence = amountofNumbers;
         number1 = num1;
         number2 = num2;
     }
@@ -15,20 +17,34 @@ public class Fibonaccii implements Fibonacci{
         int num1 = number1;
         int num2 = number2;
 
-        System.out.print(number1 + " ");
-        System.out.print(number2 + " ");
+        System.out.println(Thread.currentThread().getName() + " " + number1 + " ");
+        System.out.println(Thread.currentThread().getName() + " " + number2 + " ");
 
 
-        for (int i = 1; i <= amountofNumbersInSequence-2; i++ ) {
+        for (int i = 1; i <= amountofNumbersInSequence - 2; i++) {
             int numberX = num1 + num2;
-            num1= num2;
-            num2=numberX;
-            System.out.print(numberX + " ");
+            num1 = num2;
+            num2 = numberX;
+            System.out.println(Thread.currentThread().getName() + " " + numberX + " ");
+            //sequence = sequence + " " + numberX;
 
         }
 
-        System.out.println("DONE! printed " + amountofNumbersInSequence + "numbers!");
+        //print();
 
+    }
 
+    private void print() {
+        System.out.println("DONE! printed " + amountofNumbersInSequence + " numbers!");
+        System.out.print(Thread.currentThread().getName());
+        for (int i = 0; i < amountofNumbersInSequence -2; i++) {
+            String[] strings = sequence.split(" ");
+            System.out.print(strings[i] + " ");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
     public static void main(String args[]) {
-        Fibonaccii myFib = new Fibonaccii(10, 2, 6);
-        myFib.run();
+        Fibonaccii myFib1 = new Fibonaccii(10, 2, 6);
+        Thread myFib1Thread = new Thread(myFib1, "Sal1");
+        myFib1Thread.start();
+        //myFib1.run();
 
         Fibonaccii myFib2 = new Fibonaccii(3, 5, 6);
-        myFib2.run();
+        Thread myFib2Thread = new Thread(myFib2, "Sal2");
+        myFib2Thread.start();
+        //myFib2.run();
+
+        Fibonaccii myFib3 = new Fibonaccii(8, -1, 3);
+        Thread myFib3Thread = new Thread(myFib3, "Sal3");
+        myFib3Thread.start();
+        //myFib3.run();
+
+        Fibonaccii myFib4 = new Fibonaccii(8, 9, 15);
+        Thread myFib4Thread = new Thread(myFib4, "Sal4");
+        myFib4Thread.start();
+        //myFib4.run();
+
 
     }
 }
